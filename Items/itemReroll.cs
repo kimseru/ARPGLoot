f25c90c661a61e4d9dac82e6d22595978aafb308
kimseru <frostxmyst@gmail.com>
2017-07-04 02:41:36 -0400

    Add files via upload

---
+++ Items/itemReroll.cs
@@ -0,0 +1,56 @@
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace ARPGLoot.Items
+{
+    public class itemReroll : ModItem
+    {
+        public override bool CanRightClick()
+        {
+            if (Main.LocalPlayer.GetModPlayer<ARPGPlayer>(mod).canReroll)
+                return true;
+            return false;
+        }
+
+        public override void RightClick(Player player)
+        {
+            Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/identify").WithVolume(1f));
+            player.GetModPlayer<ARPGPlayer>(mod).reroll = true;
+            player.GetModPlayer<ARPGPlayer>(mod).canReroll = false;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Essence of Favorable Fate");
+            Tooltip.SetDefault("Right click to reroll a single item" + "\n" + "Selects top left items first" + "\n" + "Consumable");
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+
+            recipe.AddIngredient(ItemID.BottledWater, 1);
+            recipe.AddIngredient(ItemID.Ectoplasm, 1);
+            recipe.AddIngredient(ItemID.SoulofSight, 1);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 34;
+            item.height = 34;
+            item.value = 100000;
+            item.rare = 8;
+            item.maxStack = 999;
+        }
+    }
+}
---
+++ Items/magicUpgrade.cs
@@ -0,0 +1,63 @@
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace ARPGLoot.Items
+{
+    public class magicUpgrade : ModItem
+    {
+        public override bool CanRightClick()
+        {
+            if (Main.LocalPlayer.GetModPlayer<ARPGPlayer>(mod).canMagicUpgrade)
+                return true;
+            return false;
+        }
+
+        public override void RightClick(Player player)
+        {
+            Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/legendary").WithVolume(1f));
+            player.GetModPlayer<ARPGPlayer>(mod).magicClarity = true;
+            player.GetModPlayer<ARPGPlayer>(mod).canMagicUpgrade = false;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Orb of Alchemy");
+            Tooltip.SetDefault("Right click to upgrade a single magical item to rare quality" + "\n" + "Selects top left items first" + "\n" + "Consumable");
+            Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(10, 5));
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+
+            recipe.AddIngredient(1332, 1);  //soul of night
+            recipe.AddIngredient(521, 3);   //cursed flames
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(mod);
+            recipe.AddIngredient(1332, 1); 
+            recipe.AddIngredient(522, 3);   //ichor
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 30;
+            item.value = 50000;
+            item.rare = 6;
+            item.maxStack = 999;
+        }
+    }
+}
